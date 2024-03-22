59fe58959647d4705af3484f1f7bbdfd4a178730
DeepikaSolanki60 <163406388+DeepikaSolanki60@users.noreply.github.com>
2024-03-22 16:27:52 -0400

    delete functionality added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,54 @@
-import {useState} from 'react'; 
-
-function App() 
-{
-
-  const [todo, setTodo]=useState([])
-  const addToDo=() =>
-  {
-    let toDoText=document.getElementById("toDoInput").value
-    console.log(toDoText);
-    todo.push(toDoText)
-    setTodo([...todo])
-    console.log("todolenght",todo.length);
-  }
-  
-  return (
-    <div>
-
-      <h1>To-Do App</h1>
-      <input type='text'id='toDoInput' placeholder="Add a new to-do"/>
-      <button onClick={addToDo}>Add</button><br/>
-      {todo.map(tempTodo =>
+    import {useState} from 'react'; 
+    import React from 'react';
+    var count = 1
+    function App() 
+    {
+      var [todo, setTodo] = useState([])
+      const addToDo = () =>
+      {
+        console.log("Add todo");
+        const todoText=document.getElementById("todoInput").value
+        let todoObject = {
+          id: count++,
+          title: todoText
+        }
+        console.log("todotext"+todoText);
+        todo.push(todoObject)
+        setTodo([...todo])
+      }
+        const  deleteToDo = (id)  =>
         {
-          return <div>{tempTodo}</div>
-        })}
-    </div>
-  )
-}
+          console.log("Delete todo id :",id);
+          todo = todo.filter(todoTemp =>
+            {
+              if(id === todoTemp.id)
+              {
+                return false
+              }
+              else
+              {
+                return true
+              }
+            })
+            setTodo([...todo])
+        }
+      
+      
+      return (
+        <div>
+          <h1>To-Do App</h1>
+          <input type='text'id='todoInput' placeholder="Add a new to-do"/>
+          <button onClick={addToDo}>Add</button><br/>
+          {todo.map(toDoTemp =>
+            {
+              return <div>
+                {toDoTemp.id}
+                <button onClick={() =>deleteToDo(toDoTemp.id)}>Delete</button>
+              </div>
+            })}
+            {/* {getDynamicList} */}
+        </div>
+      )
+    }
 
-export default App;
+    export default App;
